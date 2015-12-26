e173e411e3360692eafb417403a4fb6bbcddf0d9
Fredrik Larsson <flarsson93@gmail.com>
2015-12-26 17:11:08 +0100

    Dont store event name

--- competition/src/competition/Result.java
+++ competition/src/competition/Result.java
@@ -1,12 +1,10 @@
 package competition;
 
 public class Result {
-	private String name;
 	private double results;
 	private Participant participant;
 	
 	public Result(Participant inputParticipant, String eName, double result){
-		name = eName;
 		results = result;
 		participant = inputParticipant;
 	}
@@ -18,11 +16,8 @@ public class Result {
 	public Participant getParticipant(){
 		return participant;
 	}
-	public String getNameOfEvent(){
-		return name;
-	}
 	@Override
 	public String toString(){
-		return "Result:"+results+" Event name:"+name+" participant ID: "+participant.getID();
+		return "Result:"+results+" Event name:"+" participant ID: "+participant.getID();
 	}
 }
