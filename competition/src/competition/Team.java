1747603f01778ba7a102cb86ced378eb4104bf86
Fredrik Larsson <flarsson93@gmail.com>
2015-12-28 21:07:29 +0100

    no hard code medals

--- competition/src/competition/Team.java
+++ competition/src/competition/Team.java
@@ -5,27 +5,13 @@ import java.util.ArrayList;
 public class Team {
 	private ArrayList<Participant> participants = new ArrayList<Participant>();
 	String name;
-	int[] medals;
 	
-	public Team(String teamName, int[] teamMedals){
-		name = teamName;
-		medals = teamMedals;		
-	}
 	public Team(String teamName){
 		name = teamName;				
 	}
 	
 	public String getTeamName(){
 		return name;
-	}
-	//0 = gold | 1 = silver | 2 = bronze
-	public void addMedal(int medalType){
-		medals[medalType]++;
-	}
-	public void addMedalByArray(int[] newMedals){
-		for(int i = 0; i<newMedals.length;i++){
-			medals[i]+=newMedals[i];
-		}
 	}
 	//removes given participant from participants and returns true if the array is empty
 	public boolean removeParticipant(Participant p){
