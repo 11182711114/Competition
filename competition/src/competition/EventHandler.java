8554afc884ab05701fda6acf4b3190bc22124a33
Fredrik Larsson <flarsson93@gmail.com>
2015-12-30 16:41:24 +0100

    return has no point and will break the method

--- competition/src/competition/EventHandler.java
+++ competition/src/competition/EventHandler.java
@@ -421,13 +421,10 @@ public class EventHandler {
 			}
 		}
 	}
-	public boolean removeResultsByParticipant(Participant p){
+	public void removeResultsByParticipant(Participant p){
 		for(Event e : events){
-			if(!(e.removeResultsByParticipant(p).isEmpty())){
-				return true;
-			}
+			e.removeResultsByParticipant(p);
 		}
-		return false;
 	}
 	public boolean reinitialize(){
 		events.clear();
